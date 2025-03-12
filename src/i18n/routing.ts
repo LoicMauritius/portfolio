cda13b234796079e9be790c14b34f8ff934d0f99
LoicMauritius <loicmauritius@gmail.com>
2025-03-12 14:44:20 +0100

    feat: Add a new route

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,6 +4,23 @@ export const routing = defineRouting({
     locales: ['en', 'fr'],
     defaultLocale: 'en',
     pathnames: {
+        "/": "/",
+        "/web": {
+            en: "/web",
+            fr: "/web"
+        },
+        "/mobile": {
+            en: "/mobile",
+            fr: "/mobile"
+        },
+        "/hobbie": {
+            en: "/hobbies",
+            fr: "/loisirs"
+        },
+        "/other": {
+            en: "/others",
+            fr: "/autres"
+        },
         "/contact": {
             en: "/contact-me",
             fr: "/contactez-moi"
@@ -12,3 +29,5 @@ export const routing = defineRouting({
 });
 
 export type Locale = (typeof routing.locales)[number];
+export type Pathnames = keyof typeof routing.pathnames;
+export const navItems: Pathnames[] = ["/", "/web", "/mobile", "/hobbie", "/other", "/contact"];
